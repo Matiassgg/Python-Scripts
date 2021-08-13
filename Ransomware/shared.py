bd22301553db71ae43a2212a916445f766f04772
matiassgg <matugrillo@hotmail.com>
2021-08-13 17:40:58 -0300

    changing on format

--- Ransomware/shared.py
+++ Ransomware/shared.py
@@ -1,9 +1,11 @@
 import os
 
-# nt => "windows" || posix => "linux" 
+# nt => "windows" || posix => "linux"
+
+
 def get_path_mode_on_so():
     character = ''
-    so_name = os.name 
+    so_name = os.name
     if so_name == "nt":
         character = '\\'
     elif so_name == "posix":
